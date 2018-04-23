731eae1b7e606dadc7f260ebcf11aaa98875e731
Hugo Andres Torres Linares <hugo.torres@globant.com>
2018-04-23 17:45:50 -0500

    working base

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  title = "Chat in whatsapp";
+  phoneNumber = { countrycode: "57", number: "" };
 
-  constructor(public navCtrl: NavController) {
+  callNumber = function() {
+    var link = document.createElement("a");
+    link.href ="tel:"+this.phoneNumber.number;
+    link.target = "_blank";
+    link.click();
+  };
+  chatnumber = function() {
+    var link = document.createElement("a");
+    link.href =
+      "https://api.whatsapp.com/send?phone=" +
+      this.phoneNumber.countrycode +
+      this.phoneNumber.number;
+    link.target = "_blank";
+    link.click();
+  };
 
-  }
-
+  constructor(public navCtrl: NavController) {}
 }
